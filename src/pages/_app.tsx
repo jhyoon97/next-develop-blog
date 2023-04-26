86bf03f5d004c754b033a335cf5ba4a491837fd2
YoonJihwan <jecgdd@naver.com>
2023-04-26 18:41:44 +0900

    * reset css

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,14 @@ import "react-notion-x/src/styles.css";
 import "prismjs/themes/prism-tomorrow.css";
 import type { AppProps } from "next/app";
 import { QueryClient, QueryClientProvider } from "react-query";
+import { Global } from "@emotion/react";
 
 // dayjs
 import dayjs from "dayjs";
 import timezone from "dayjs/plugin/timezone";
 
 // utils
+import resetCss from "utils/resetCss";
 import initAxios from "utils/axios";
 
 dayjs.extend(timezone);
@@ -20,6 +22,7 @@ const client = new QueryClient();
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <QueryClientProvider client={client}>
+      <Global styles={resetCss} />
       <Component {...pageProps} />
     </QueryClientProvider>
   );
