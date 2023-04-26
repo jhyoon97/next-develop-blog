6eb8889b21ba2d6c406400de7a2c714e850f31d0
YoonJihwan <jecgdd@naver.com>
2023-04-26 18:59:05 +0900

    * reset css https://velog.io/@teo/2022-CSS-Reset-%EB%8B%A4%EC%8B%9C-%EC%8D%A8%EB%B3%B4%EA%B8%B0

--- src/utils/resetCss.ts
+++ src/utils/resetCss.ts
@@ -1,130 +1,51 @@
 import { css } from "@emotion/react";
 
 export default css`
-  html,
-  body,
-  div,
-  span,
-  applet,
-  object,
-  iframe,
-  h1,
-  h2,
-  h3,
-  h4,
-  h5,
-  h6,
-  p,
-  blockquote,
-  pre,
-  a,
-  abbr,
-  acronym,
-  address,
-  big,
-  cite,
-  code,
-  del,
-  dfn,
-  em,
-  img,
-  ins,
-  kbd,
-  q,
-  s,
-  samp,
-  small,
-  strike,
-  strong,
-  sub,
-  sup,
-  tt,
-  var,
-  b,
-  u,
-  i,
-  center,
-  dl,
-  dt,
-  dd,
-  ol,
-  ul,
-  li,
-  fieldset,
-  form,
-  label,
-  legend,
-  table,
-  caption,
-  tbody,
-  tfoot,
-  thead,
-  tr,
-  th,
-  td,
-  article,
-  aside,
-  canvas,
-  details,
-  embed,
-  figure,
-  figcaption,
-  footer,
-  header,
-  hgroup,
-  menu,
-  nav,
-  output,
-  ruby,
-  section,
-  summary,
-  time,
-  mark,
-  audio,
-  video {
+  * {
     margin: 0;
     padding: 0;
-    border: 0;
-    font-size: 100%;
-    vertical-align: baseline;
+    font: inherit;
+    color: inherit;
+    font-family: "Noto Sans KR", sans-serif;
   }
-  /* HTML5 display-role reset for older browsers */
-  article,
-  aside,
-  details,
-  figcaption,
-  figure,
-  footer,
-  header,
-  hgroup,
-  menu,
-  nav,
-  section {
-    display: block;
+  *,
+  :after,
+  :before {
+    box-sizing: border-box;
+    flex-shrink: 0;
   }
+  :root {
+    -webkit-tap-highlight-color: transparent;
+    -webkit-text-size-adjust: 100%;
+    text-size-adjust: 100%;
+    cursor: default;
+    line-height: 1.5;
+    overflow-wrap: break-word;
+    word-break: break-word;
+    tab-size: 4;
+  }
+  html,
   body {
-    line-height: 1;
+    height: 100%;
   }
-  ol,
-  ul {
-    list-style: none;
+  img,
+  picture,
+  video,
+  canvas,
+  svg {
+    display: block;
+    max-width: 100%;
   }
-  blockquote,
-  q {
-    quotes: none;
+  button {
+    background: none;
+    border: 0;
+    cursor: pointer;
   }
-  blockquote:before,
-  blockquote:after,
-  q:before,
-  q:after {
-    content: "";
-    content: none;
+  a {
+    text-decoration: none;
   }
   table {
     border-collapse: collapse;
     border-spacing: 0;
-  }
-  * {
-    font-family: "Noto Sans KR", sans-serif;
   }
 `;
