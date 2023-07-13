43134b2cf7c292f8c30d62da05d8fa53c7d51a20
YoonJihwan <jecgdd@naver.com>
2023-07-13 23:04:56 +0900

    * import error 해결

--- src/pages/api/post/[postId].ts
+++ src/pages/api/post/[postId].ts
@@ -1,14 +1,11 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
-// utils
-import { getPage } from "utils/notion";
-
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === "GET") {
     const { postId } = req.query;
 
-    const page = await getPage(postId as string);
+    // const page = await getPage(postId as string);
 
-    res.status(200).json(page);
+    res.status(200).json({});
   }
 };
