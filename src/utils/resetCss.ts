40bc260d18d7ea73ae70e7fc16d15ea2f5f8b3af
YoonJihwan <jecgdd@naver.com>
2023-08-25 00:24:01 +0900

    * resetCss - 모든요소 font-size inherit으로 수정

--- src/utils/resetCss.ts
+++ src/utils/resetCss.ts
@@ -10,7 +10,7 @@ export default (theme: Theme) => css`
     font: inherit;
     color: inherit;
     font-family: "Noto Sans KR", sans-serif;
-    font-size: 1rem;
+    font-size: inherit;
   }
   code[class*="language-"] * {
     font-family: inherit;
