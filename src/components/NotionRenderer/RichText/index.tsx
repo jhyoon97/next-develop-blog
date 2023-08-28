39ffa3753e1f25a95bc301de0d586f1983b72d0e
YoonJihwan <jecgdd@naver.com>
2023-08-28 20:42:57 +0900

    NotionRenderer - RichText: nestedRichText 코드 중복 제거

--- src/components/NotionRenderer/RichText/index.tsx
+++ src/components/NotionRenderer/RichText/index.tsx
@@ -23,19 +23,64 @@ interface Props {
   richText: Array<RichTextItemResponse>;
 }
 
-interface LinkRichTextInCode {
+interface LinkRichTextGroupInCode {
   groupType: "link";
   richText: Array<RichTextItemResponse>;
 }
 
+type RichTextGroupItem = RichTextItemResponse | LinkRichTextGroupInCode;
+
 interface RichTextGroup {
   groupType: "code" | "link";
-  richText: Array<RichTextItemResponse | LinkRichTextInCode>;
+  richText: Array<RichTextGroupItem>;
 }
 
 export type NestedRichTextItem = RichTextItemResponse | RichTextGroup;
 
 type NestedRichText = Array<NestedRichTextItem>;
+
+const groupingLink = (array: Array<NestedRichTextItem | RichTextGroupItem>) => {
+  // href 값이 같은 블록들을 그룹핑하는 함수
+  return array.reduce<Array<NestedRichTextItem | RichTextGroupItem>>(
+    (acc, item, index, arr) => {
+      if (
+        "href" in item &&
+        item.href &&
+        (item.annotations.code ===
+          (arr[index + 1] as RichTextItemResponse)?.annotations?.code ||
+          index + 1 === arr.length)
+      ) {
+        const lastAccItem = acc[acc.length - 1];
+
+        if (
+          lastAccItem &&
+          "groupType" in lastAccItem &&
+          item.href === (arr[index - 1] as RichTextItemResponse)?.href
+        ) {
+          // 그룹과 href값이 같은 경우에만 포함
+          const newAcc = [...acc];
+
+          (newAcc[newAcc.length - 1] as RichTextGroup).richText.push(item);
+
+          return newAcc;
+        }
+
+        if (
+          "href" in item &&
+          item.href === (arr[index + 1] as RichTextItemResponse)?.href
+        ) {
+          // 다음 아이템의 href값과 일치하면 그룹 생성
+          return acc.concat({ groupType: "link", richText: [item] });
+        }
+
+        return acc.concat(item);
+      }
+
+      return acc.concat(item);
+    },
+    []
+  );
+};
 
 const RichText = ({ richText }: Props) => {
   const nestedRichText = useMemo<NestedRichText>(() => {
@@ -62,96 +107,14 @@ const RichText = ({ richText }: Props) => {
       []
     );
 
-    const groupByLink = groupByCode.reduce<NestedRichText>(
-      (acc, item, index, arr) => {
-        if (
-          "href" in item &&
-          item.href &&
-          (item?.annotations?.code ===
-            (arr[index + 1] as RichTextItemResponse)?.annotations?.code ||
-            index + 1 === arr.length)
-        ) {
-          // 코드로 감싸져있지 않은 링크
-          const lastAccItem = acc[acc.length - 1];
-
-          if (
-            lastAccItem &&
-            "groupType" in lastAccItem &&
-            item.href === (arr[index - 1] as RichTextItemResponse)?.href
-          ) {
-            // 그룹과 href값이 같은 경우에만 포함
-            const newAcc = [...acc];
-
-            (newAcc[newAcc.length - 1] as RichTextGroup).richText.push(item);
-
-            return newAcc;
-          }
-
-          if (
-            "href" in item &&
-            item.href === (arr[index + 1] as RichTextItemResponse)?.href
-          ) {
-            // 다음 아이템의 href값과 일치하면 그룹 생성
-            return acc.concat({ groupType: "link", richText: [item] });
-          }
-
-          return acc.concat(item);
-        }
-
-        return acc.concat(item);
-      },
-      []
-    );
+    const groupByLink = groupingLink(groupByCode);
 
     const groupByLinkInCode = groupByLink.map((item) => {
       if ("groupType" in item && item.groupType === "code") {
-        // 코드 내의 링크
         return {
-          ...item,
-          richText: item.richText.reduce<
-            Array<RichTextItemResponse | LinkRichTextInCode>
-          >((acc, innerItem, index, arr) => {
-            if (
-              "href" in innerItem &&
-              innerItem.href &&
-              (innerItem.annotations.code ===
-                (arr[index + 1] as RichTextItemResponse)?.annotations.code ||
-                index + 1 === arr.length)
-            ) {
-              // 코드로 감싸져있지 않은 링크
-              const lastAccItem = acc[acc.length - 1];
-
-              if (
-                lastAccItem &&
-                "groupType" in lastAccItem &&
-                innerItem.href ===
-                  (arr[index - 1] as RichTextItemResponse)?.href
-              ) {
-                // 그룹과 href값이 같은 경우에만 포함
-                const newAcc = [...acc];
-
-                (newAcc[newAcc.length - 1] as RichTextGroup).richText.push(
-                  innerItem
-                );
-
-                return newAcc;
-              }
-
-              if (
-                "href" in innerItem &&
-                innerItem.href ===
-                  (arr[index + 1] as RichTextItemResponse)?.href
-              ) {
-                // 다음 아이템의 href값과 일치하면 그룹 생성
-                return acc.concat({ groupType: "link", richText: [innerItem] });
-              }
-
-              return acc.concat(innerItem);
-            }
-
-            return acc.concat(innerItem);
-          }, []),
-        };
+          groupType: item.groupType,
+          richText: groupingLink(item.richText),
+        } as RichTextGroup;
       }
 
       return item;
@@ -159,8 +122,6 @@ const RichText = ({ richText }: Props) => {
 
     return groupByLinkInCode;
   }, [richText]);
-
-  console.log(nestedRichText);
 
   return (
     <>
