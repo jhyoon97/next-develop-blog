0095ea01f40d92b6f9e4a03bde6a041e7c32c67b
YoonJihwan <jecgdd@naver.com>
2023-09-04 19:16:45 +0900

    NotionRenderer 커밋 누락

--- src/components/NotionRenderer/index.tsx
+++ src/components/NotionRenderer/index.tsx
@@ -3,27 +3,29 @@ import { css } from "@emotion/react";
 // types
 import type { HasChildrenBlockObject } from "@types";
 
-import Paragraph from "./Paragraph";
 import HeadingLevel1 from "./HeadingLevel1";
 import HeadingLevel2 from "./HeadingLevel2";
 import HeadingLevel3 from "./HeadingLevel3";
 import Code from "./Code";
 import Image from "./Image";
+import Paragraph from "./Paragraph";
 
 interface Props {
   blocks: HasChildrenBlockObject[];
+  depth?: number;
 }
 
-const box = css`
+const box = (depth: number) => css`
   display: flex;
   flex-direction: column;
   align-items: flex-start;
+  padding-left: ${depth * 1}rem;
   width: 100%;
 `;
 
-const NotionRenderer = ({ blocks }: Props) => {
+const NotionRenderer = ({ blocks, depth = 1 }: Props) => {
   return (
-    <div css={box}>
+    <div css={box(depth)}>
       {blocks.map((block) =>
         (() => {
           switch (block.type) {
